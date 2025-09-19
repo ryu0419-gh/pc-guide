13c570d2cfc3769c8976985dce9995ff3f70afb9
Ura20 <ura2091.212@gmail.com>
2025-09-20 08:05:06 +0900

    test

--- components/organisms/GameCard.tsx
+++ components/organisms/GameCard.tsx
@@ -1,3 +1,41 @@
-export default function GameCard() {
-  return <div className="border p-2">ダミーゲームカード</div>;
-}
+"use client";
+
+import React from "react";
+import { Box, Heading,Text } from "@chakra-ui/react";
+import Image from "next/image";
+import { GameProps } from "@/type/type";
+
+type Props = {
+  game: GameProps;
+  onSelect: (id: string) => void;
+};
+
+export const GameCard = ({ game, onSelect }: Props) => {
+  return (
+    <Box
+      borderWidth="1px"
+      borderRadius="md"
+      overflow="hidden"
+      boxShadow="md"
+      _hover={{ boxShadow: "lg", bg: "gray.700" }}
+      p={3}
+      cursor="pointer"
+      onClick={() => onSelect(game.id)}
+      bg="gray.900"
+      color="white"
+    >
+      <Box position="relative" w="100%" h="200px">
+        <Image
+          src={game.thumbnail}
+          alt={game.title}
+          fill
+          style={{ objectFit: "cover", borderRadius: "0.375rem" }}
+        />
+      </Box>
+      <Heading as="h3" size="md" mt={2} fontFamily="'Staatliches', sans-serif">
+        {game.title}
+      </Heading>
+      <Text>これはテストです</Text>
+    </Box>
+  );
+};
